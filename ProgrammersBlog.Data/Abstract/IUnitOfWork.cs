0d2123f8ab3455882872f50e9b1d1f9594cb36ee
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-06-29 15:00:41 +0300

    IUnitOfWork interface and UnitOfWork class created.

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get;  } //unitOfWork.Articles
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } // _unitOfWork.Categories.AddAsync();
+        //_unitOfWork.Categories.AddAsync(category);
+        //_unitOfWork.Categories.AddAsync(user);
+        //_unitOfWork.SaveAsync();
+        Task<int> SaveASync();
+    }
+}
