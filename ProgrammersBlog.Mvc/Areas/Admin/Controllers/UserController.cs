d97b72ed455a802b7288f7774b28ebf5b30ceff6
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-07-07 23:56:30 +0300

    ImageUpload method created.

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
@@ -15,10 +16,12 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
 
-        public UserController(UserManager<User> userManager)
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env)
         {
             _userManager = userManager;
+            _env = env;
         }
         public async Task<IActionResult> Index()
         {
@@ -33,6 +36,25 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
         public IActionResult Add()
         {
             return PartialView("_UserAddPartial");
+        }
+
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            // ~/img/user.Picture
+            string wwwroot = _env.WebRootPath;
+            //cihanzumrut
+            //string fileName2 = Path.GetFileNameWithoutExtension(userAddDto.Picture.FileName);
+            //.png
+            string fileExtension = Path.GetExtension(userAddDto.Picture.FileName);
+            DateTime datetime = DateTime.Now;
+            //CihanZumrut_587_5_38_12_3_10_2021.png
+            string fileName = $"{userAddDto.UserName}_{datetime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path = Path.Combine($"{wwwroot}/img, fileName");
+            await using (var stream = new FileStream(path,FileMode.Create))
+            {
+                await userAddDto.Picture.CopyToAsync(stream);
+            }
+            return fileName; ///CihanZumrut_587_5_38_12_3_10_2021.png - "~/img/user.Picture"
         }
     }
 }
---
+++ ProgrammersBlog.Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime datetime)
+        {
+            return $"{datetime.Millisecond}_{datetime.Second}_{datetime.Minute}_{datetime.Hour}_{datetime.Day}_{datetime.Month}_{datetime.Year}";
+
+            /*
+             *CihanZumrut_587_5_38_12_3_10_2021_CihanZumrutResmi.png
+             *AliVeli_601_5_38_12_3_10_2021.png
+             */
+        }
+    }
+}
