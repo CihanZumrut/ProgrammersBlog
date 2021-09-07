f67c66b54912149b62b1eebc10eba2a98f144682
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-09-07 14:16:44 +0300

    UserLoginDto created.

---
+++ ProgrammersBlog.Entities/Dtos/UserLoginDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserLoginDto
+    {
+        [DisplayName("E-Posta Adresi")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(10, ErrorMessage = "{0} {1} karakterden az olmamalıdır..")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden az olmamalıdır..")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
