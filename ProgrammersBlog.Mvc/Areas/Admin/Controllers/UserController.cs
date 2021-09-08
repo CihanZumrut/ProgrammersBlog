23c2d606744edc8a82392a7639098545df94a192
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-09-08 13:42:22 +0300

    ChangeDetails HttpPost action

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -276,6 +276,50 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             return View(updateDto);
         }
 
+        [HttpPost]
+        public async Task<ViewResult> ChangeDetails(UserUpdateDto userUpdateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isNewPictureUploaded = false;
+                var oldUser = await _userManager.GetUserAsync(HttpContext.User);
+                var oldUserPicture = oldUser.Picture;
+                if (userUpdateDto.PictureFile != null)
+                {
+                    userUpdateDto.Picture = await ImageUpload(userUpdateDto.UserName, userUpdateDto.PictureFile);
+                    if (oldUserPicture != "defaultUser.png")
+                    {
+                        isNewPictureUploaded = true;
+                    }
+                }
+
+                var updatedUser = _mapper.Map<UserUpdateDto, User>(userUpdateDto, oldUser);
+                var result = await _userManager.UpdateAsync(updatedUser);
+                if (result.Succeeded)
+                {
+                    if (isNewPictureUploaded)
+                    {
+                        ImageDelete(oldUserPicture);
+                    }
+                    TempData.Add("SuccessMessage", $"{updatedUser.UserName} adlı kullanıcı başarıyla güncellenmiştir.");
+                    return View(userUpdateDto);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(userUpdateDto);
+                }
+            }
+            else
+            {
+                return View(userUpdateDto);
+            }
+        }
+
 
         [Authorize(Roles = "Admin, Editor")]
         public async Task<string> ImageUpload(string userName, IFormFile pictureFile)
