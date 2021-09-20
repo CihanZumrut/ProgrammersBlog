a2aee59d2a7e238f8cf1c0ac80f1588bc7593fb7
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-09-20 14:10:18 +0300

    Some bug fixed.

--- ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -30,6 +30,7 @@ namespace ProgrammersBlog.Services.Extensions
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();
+            serviceCollection.AddScoped<ICommentService, CommentManager>();
             return serviceCollection;
         }
 
