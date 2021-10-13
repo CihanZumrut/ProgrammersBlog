a34ceeb74d7f79fdd1292ff75edc947826860025
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-10-13 15:44:18 +0300

    Users class and pages extended. Also new migration.

--- ProgrammersBlog.Entities/Concrete/User.cs
+++ ProgrammersBlog.Entities/Concrete/User.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
-using ProgrammersBlog.Shared.Entities.Abstract;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProgrammersBlog.Entities.Concrete
 {
-    public class User : IdentityUser<int>
+    public class User:IdentityUser<int>
     {
         public string Picture { get; set; }
         public ICollection<Article> Articles { get; set; }
+        public string About { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string YoutubeLink { get; set; }
+        public string TwitterLink { get; set; }
+        public string InstagramLink { get; set; }
+        public string FacebookLink { get; set; }
+        public string LinkedInLink { get; set; }
+        public string GitHubLink { get; set; }
+        public string WebsiteLink { get; set; }
     }
 }
