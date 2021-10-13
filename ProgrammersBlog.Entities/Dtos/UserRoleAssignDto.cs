2efb877c7014866cc0ee61fde792dcdd53dda070
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-10-13 17:00:37 +0300

    UserRoleAssignDto and RoleAssignDto created.

---
+++ ProgrammersBlog.Entities/Dtos/UserRoleAssignDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserRoleAssignDto
+    {
+        public UserRoleAssignDto()
+        {
+            RoleAssignDtos = new List<RolesAssignDto>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public IList<RolesAssignDto> RoleAssignDtos { get; set; }
+    }
+}
