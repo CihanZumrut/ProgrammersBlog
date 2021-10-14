172c36c35f0ee99b19f54963272de2dfb7a3caef
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-10-14 16:01:05 +0300

    BlogHome Index.cshtml created.

--- ProgrammersBlog.Mvc/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,17 @@ namespace ProgrammersBlog.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IArticleService _articleService;
+
+        public HomeController(IArticleService articleService)
         {
-            return View();
+            _articleService = articleService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var articleListDto = await _articleService.GetAllByNonDeletedAndActiveAsync();
+            return View(articleListDto.Data);
         }
     }
 }
