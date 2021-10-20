b2a53c5174aaaa26d36c6a2fb1acddcf245cfeda
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-10-20 22:03:51 +0300

    AboutUsPageInfo created.

--- ProgrammersBlog.Mvc/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Services.Abstract;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,12 @@ namespace ProgrammersBlog.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly IArticleService _articleService;
+        private readonly AboutUsPageInfo _aboutUsPageInfo;
 
-        public HomeController(IArticleService articleService)
+        public HomeController(IArticleService articleService, IOptions<AboutUsPageInfo> aboutUsPageInfo)
         {
             _articleService = articleService;
+            _aboutUsPageInfo = aboutUsPageInfo.Value;
         }
         [HttpGet]
         public async Task<IActionResult> Index(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
@@ -22,6 +26,11 @@ namespace ProgrammersBlog.Mvc.Controllers
                 ? _articleService.GetAllByPagingAsync(null, currentPage, pageSize, isAscending)
                 : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize, isAscending));
             return View(articlesResult.Data);
+        }
+        [HttpGet]
+        public async Task<IActionResult> About()
+        {
+            return View(_aboutUsPageInfo);
         }
     }
 }
