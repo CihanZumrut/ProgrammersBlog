47dbfa98f974017a7fe92168943899f665003a1b
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-10-20 13:30:52 +0300

    CommentAddAjaxViewModel, CommentController created.

---
+++ ProgrammersBlog.Mvc/Models/CommentAddAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProgrammersBlog.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc.Models
+{
+    public class CommentAddAjaxViewModel
+    {
+        public CommentAddDto CommentAddDto { get; set; }
+        public string CommentAddPartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
