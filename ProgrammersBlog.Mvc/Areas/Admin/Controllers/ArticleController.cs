908383388d920adc124a189a502bad87b8ac23ef
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2022-02-01 12:26:49 +0300

    Chart.js library added.

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/ArticleController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/ArticleController.cs
@@ -196,5 +196,16 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             var hardDeletedArticleResult = JsonSerializer.Serialize(result);
             return Json(hardDeletedArticleResult);
         }
+        [Authorize(Roles ="SuperAdmin,Article.Read")]
+        [HttpGet]
+        public async Task<JsonResult> GetAllByViewCount(bool isAscending, int takeSize)
+        {
+            var result = await _articleService.GetAllByViewCountAsync(isAscending, takeSize);
+            var articles = JsonSerializer.Serialize(result.Data.Articles, new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+            return Json(articles);
+        }
     }
 }
